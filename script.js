5b37df4f43d8e750629bb0e05a54c388f416ad80
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-05-21 08:16:13 +0530

    Number prime check optimised

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 function isPrime(n) {
-   if(n <=1)  {
-      return false;
-   }
+  if (n <= 1) return false;
+  if (n <= 3) return true; 
 
-   if( n == 2 || n == 3) {
-      return false;
-   }
+  if (n % 2 === 0 || n % 3 === 0) return false;
 
-   for(let i=2; i * i < n; i++) {
-      if( n % i == 0) {
-         return false;
-      }
-   }
-
-   return true;
+  for (let i = 5; i * i <= n; i = i + 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+  return true;
 }
-
-console.log(isPrime(4));
